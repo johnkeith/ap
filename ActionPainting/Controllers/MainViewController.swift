303519c9dd3ca9a6b1932e024e540d99ba356115
John Keith <johnkeith@outlook.com>
2018-11-23 16:30:49 -0500

    init commit; removed storyboard, app delegate points to main vc

---
+++ ActionPainting/Controllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  ActionPainting
+//
+//  Created by John Keith on 11/23/18.
+//  Copyright © 2018 John Keith. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.purple
+    }
+}
